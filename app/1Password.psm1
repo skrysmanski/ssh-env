aec8a643475e640bb7a6e735e218bae896b8d224
Sebastian Krysmanski <noreply@manski.net>
2024-07-16 15:47:20 +0200

    Add configuration for 1Password's SSH agent

---
+++ app/1Password.psm1
@@ -0,0 +1,52 @@
+# Stop on every error
+$script:ErrorActionPreference = 'Stop'
+
+Import-Module "$PSScriptRoot/Utils.psm1"
+
+function Test-Is1PasswordIntegrationSupported() {
+	return Test-IsWindows -or $IsMacOs
+}
+
+function Test-Is1PasswordInstalled() {
+	if (-Not (Test-Is1PasswordIntegrationSupported)) {
+		return $false
+	}
+
+	if (Get-Process '1password' -ErrorAction SilentlyContinue)
+	{
+		return $true
+	}
+	else {
+		return $false
+	}
+}
+Export-ModuleMember -Function Test-Is1PasswordInstalled
+
+function Get-1PasswordSettingsFilePath() {
+	if (Test-IsWindows) {
+		return "$([Environment]::GetFolderPath('LocalApplicationData'))\1Password\settings\settings.json"
+	}
+	elseif ($IsMacOs) {
+		throw 'macOS support for 1password detection is not yet implemented'
+	}
+	else {
+		throw 'The 1Password intergration is not supported for the current operating system.'
+	}
+}
+
+function Test-Is1PasswordSshAgentEnabled() {
+	$settingsFile = Get-1PasswordSettingsFilePath
+
+	if (-Not (Test-Path $settingsFile -PathType Leaf)) {
+		return $false
+	}
+
+	$1passwordSettings = Get-Content $settingsFile | ConvertFrom-Json
+	if ($1passwordSettings.'sshAgent.enabled' -eq $true) {
+		return $true
+	}
+	else {
+		return $false
+	}
+}
+Export-ModuleMember -Function Test-Is1PasswordSshAgentEnabled
